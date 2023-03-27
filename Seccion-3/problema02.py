8705f5b3d246c19e30dd8ae76e8a290528682fa7
Sofia Genchi <sofigen16@gmail.com>
2023-03-27 16:08:31 -0300

    problema 02 iniciando

---
+++ Seccion-3/problema02.py
@@ -0,0 +1,6 @@
+# Tarea: Problema 02
+"""Crear un programa que permita convertir una cantidad de segundos en horas, minutos y segundos"""
+
+"""
+Analisis: para la solucion de este problema, se requiere que el usuario ingrese un tiempo expresado en segundos y el sistema procesa y obtiene las horas, minutos y segundos restantes.
+"""
