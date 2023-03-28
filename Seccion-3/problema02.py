cc942d1ccbd78a71c8e1627c9b273354d1eac0f3
Sofia Genchi <sofigen16@gmail.com>
2023-03-28 11:07:52 -0300

    resolviendo problema02

--- Seccion-3/problema02.py
+++ Seccion-3/problema02.py
@@ -3,4 +3,16 @@
 
 """
 Analisis: para la solucion de este problema, se requiere que el usuario ingrese un tiempo expresado en segundos y el sistema procesa y obtiene las horas, minutos y segundos restantes.
+
+Entrada: tiempo en segundos
+
+Salida: horas, minutos, segundos
 """
+
+tiempo = int(input("Ingrese la cantidad de segundos: "))
+
+horas = tiempo // 3600
+segundos = tiempo
+minutos = (horas % segundos) * 60
+
+print(f"{tiempo} segundos son {horas} horas, {minutos} minutos y {segundos} segundos")
