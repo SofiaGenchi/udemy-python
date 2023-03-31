06e106a621512533032c072e4c071d0d885de22e
Sofia Genchi <sofigen16@gmail.com>
2023-03-31 15:52:17 -0300

    aplicacion conversor de monedas

--- Seccion-8/conversor.py
+++ Seccion-8/conversor.py
@@ -7,3 +7,37 @@ Para solucionar este problema se requiere que el usuario ingrese la cantidad de 
 Para este sistemadebe hacer un menu de navgecion para seleccionar a que tipo de moneda se hara la conversion y tambien para cerrar el programa, el sistema no se debe cerrar si no se desea.
 """
 
+def conversor(valor_dolar, pais):
+    cantidad_moneda = float(input(f'Ingrese cantidad de {pais}: '))
+
+    dolares = cantidad_moneda / valor_dolar
+    dolares = round(dolares, 2)
+    print(f'Tienes $${dolares} Dolares')
+
+
+def main():
+    while True:
+        menu = """
+        1- Soles Peruanos a Dolares
+        2- Pesos Mexicanos a Dolares
+        3- Pesos Colombianos a Dolares
+        4- Salir
+        Ingrese una Opcion: 
+        """
+        opcion = input(menu)
+
+        if opcion == '1':
+            conversor(3.61, 'soles peruanos')
+        elif opcion == '2':
+            conversor(20.01, 'pesos mexicanos')
+        elif opcion == '3':
+            conversor(3471.27, 'Pesos Colombianos')
+        elif opcion == '4':
+            print('Cerrando conversor de monedas')
+            break
+        else:
+            print('Opcion incorrecta!')
+            print('Vuelve a ingresar la opcion correcta')
+
+if __name__ == '__main__':
+    main()
