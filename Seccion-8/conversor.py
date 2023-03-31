32e4d0eaaeb7af1a1a1c619c034a01ef45f3d999
Sofia Genchi <sofigen16@gmail.com>
2023-03-31 15:02:19 -0300

    iniciando conversor

---
+++ Seccion-8/conversor.py
@@ -0,0 +1,9 @@
+# Practica 04: Conversor de Monedas
+"""
+Crear un sistema que convierta de moneda nacional a dolares por ejemplo.
+
+Para solucionar este problema se requiere que el usuario ingrese la cantidad de mondas nacionales y luego realizar la conversion.
+
+Para este sistemadebe hacer un menu de navgecion para seleccionar a que tipo de moneda se hara la conversion y tambien para cerrar el programa, el sistema no se debe cerrar si no se desea.
+"""
+
