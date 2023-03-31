707f7d26ab00f0e524cf38c8c74828d733655a83
Sofia Genchi <sofigen16@gmail.com>
2023-03-31 12:45:00 -0300

    metodo_cadena de texto

---
+++ Seccion-7/metodo_cadena.py
@@ -0,0 +1,26 @@
+# METODO DE CADENA DE CARACTERES
+
+a = 'Hola Mundo'
+
+mayuscula = a.upper()
+print(mayuscula)
+
+minuscula = a.lower()
+print(minuscula)
+
+primer_mayuscula = a.capitalize()
+print(primer_mayuscula)
+
+titulo = a.title()
+print(titulo)
+
+contar = a.count('o')
+print(contar)
+
+reemplazar = a.replace('H', 'S')
+print(reemplazar)
+
+
+espacios = '    Hola    Mundo'
+borrar_espacios = espacios.strip()
+print(borrar_espacios)
